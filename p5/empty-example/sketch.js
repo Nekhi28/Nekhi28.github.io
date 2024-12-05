b3ae4d8fed4c6ef00d3e8c177f4f9aef5033ddaa
Nekhi28 <dmkrin69@gmail.com>
2024-12-05 15:07:39 -0500

    Add files via upload

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,12 @@
+var starX = 200;
+var starY = 100;
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  background('rgba(0, 255, 0, 0.25)');
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(starX,starY,starY,starY);
+}
